b670399a90fd7f09b5e6a401f4c40d7718c1de64
Arik Cohen <arik.x.cohen.-nd@disney.com>
2016-06-12 17:07:55 -0700

    1. working on the core abstractions.

--- src/main/java/com/creactiviti/piper/core/Worker.java
+++ src/main/java/com/creactiviti/piper/core/Worker.java
@@ -1,9 +1,35 @@
 package com.creactiviti.piper.core;
 
+/**
+ * <p>The interface to be implemented by object responsible for 
+ * executing tasks spawned by the {@link Coordinator}.</p>
+ * 
+ * <p>Implementations will typically execute on a different
+ * process than the {@link Coordinator} process and most likely
+ * on a seperate node altogether.</p>
+ * 
+ * <p>Communication between the two is decoupled through the 
+ * {@link Messager} interface.</p>
+ * 
+ * @author Arik Cohen
+ * @since Jun 12, 2016
+ *
+ */
 public interface Worker {
 
+  /**
+   * Start the execution of a {@link Task} asynchronously. 
+   * 
+   * @param aTask
+   *          The task to execute.
+   */
   void start (Task aTask);
   
+  /**
+   * Stop the execution of a running task.
+   * 
+   * @param aTaskId
+   */
   void stop (String aTaskId);
   
 }
---
+++ src/main/java/com/creactiviti/piper/core/events/web/JobController.java
@@ -0,0 +1,16 @@
+package com.creactiviti.piper.core.events.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.creactiviti.piper.core.Job;
+
+@RestController
+@RequestMapping("/job")
+public class JobController {
+
+  Job start () {
+    return null;
+  }
+  
+}
