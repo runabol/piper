66627c77e0e78db51106d0317e216313d94c2512
Arik Cohen <arik.x.cohen.-nd@disney.com>
2016-06-18 14:48:21 -0700

    1. working on core.

--- src/main/java/com/creactiviti/piper/core/Messenger.java
+++ src/main/java/com/creactiviti/piper/core/Messenger.java
@@ -1,7 +1,17 @@
 package com.creactiviti.piper.core;
 
+
+/**
+ * <p>Abstraction for sending messages between the various componentes of the application. 
+ * Implementations are responsible for the guranteed delivery of the message in a 
+ * send-and-forget fashion.</p>
+ * 
+ * @author Arik Cohen
+ * @since Jun 18, 2016
+ * 
+ */
 public interface Messenger {
 
-  void send (Object aMessage);
+  void send (String aRoutingKey, Object aMessage);
   
 }
