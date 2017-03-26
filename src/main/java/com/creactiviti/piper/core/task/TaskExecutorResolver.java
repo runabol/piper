fa01c1769c59df5a82ded74e4038e14670af6b47
Arik Cohen <arik.x.cohen.-nd@disney.com>
2017-03-26 14:02:17 -0700

    JavaDocs

--- src/main/java/com/creactiviti/piper/core/task/TaskExecutorResolver.java
+++ src/main/java/com/creactiviti/piper/core/task/TaskExecutorResolver.java
@@ -1,7 +1,26 @@
 package com.creactiviti.piper.core.task;
 
-public interface TaskExecutorResolver {
 
+/**
+ * The strategey interface used for resolving the 
+ * apprpriate {@link TaskExecutor} instance for a 
+ * given {@link JobTask}.
+ * 
+ * @author Arik Cohen
+ * @since Mar 26, 2017
+ */
+public interface TaskExecutorResolver {
+  
+  /**
+   * Resolves a {@link TaskExecutor} for the given
+   * {@link JobTask} instance or <code>null</code>
+   * if one can not be resolved. 
+   * 
+   * @param aTask
+   *           The {@link JobTask} instance
+   * @return a {@link TaskExecutor} instance to execute the given task or <code>null</code> if 
+   *         unable to resolve one. 
+   */
   TaskExecutor resolve (JobTask aTask);
   
 }
