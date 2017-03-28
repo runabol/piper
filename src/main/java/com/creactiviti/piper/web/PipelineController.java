b27b563febaca418f73b712978b2192190e0e415
creactiviti <arik.c.mail@gmail.com>
2017-03-28 13:06:56 -0700

    added API for pipelines.

---
+++ src/main/java/com/creactiviti/piper/web/PipelineController.java
@@ -0,0 +1,25 @@
+package com.creactiviti.piper.web;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.creactiviti.piper.core.pipeline.Pipeline;
+import com.creactiviti.piper.core.pipeline.PipelineRepository;
+
+@RestController
+@RequestMapping("/pipeline")
+public class PipelineController {
+
+  @Autowired
+  private PipelineRepository pipelineRepository;
+  
+  @GetMapping
+  public List<Pipeline> list () {
+    return pipelineRepository.findAll();
+  }
+  
+}
