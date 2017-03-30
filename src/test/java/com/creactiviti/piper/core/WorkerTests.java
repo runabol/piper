82034b2826723e916f41ebce3d4bbcca43263235
creactiviti <arik.c.mail@gmail.com>
2017-03-30 15:17:06 -0700

    unit tests

---
+++ src/test/java/com/creactiviti/piper/core/WorkerTests.java
@@ -0,0 +1,38 @@
+package com.creactiviti.piper.core;
+
+import java.util.Collections;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.creactiviti.piper.core.job.MutableJobTask;
+import com.creactiviti.piper.core.messenger.SynchMessenger;
+import com.creactiviti.piper.core.task.JobTask;
+
+public class WorkerTests {
+
+  @Test
+  public void test1 () {
+    Worker worker = new Worker();
+    SynchMessenger messenger = new SynchMessenger();
+    messenger.receive("completions", (t)-> Assert.assertTrue(((JobTask)t).getOutput().equals("done")) );
+    worker.setMessenger(messenger);
+    worker.setTaskHandlerResolver((jt) -> (t) -> "done");
+    worker.handle(new MutableJobTask(Collections.EMPTY_MAP));
+  }
+  
+  
+  @Test
+  public void test2 () {
+    Worker worker = new Worker();
+    SynchMessenger messenger = new SynchMessenger();
+    messenger.receive("errors", (t)-> Assert.assertTrue( ((JobTask)t).getException().getMessage().equals("bad input") ) );
+    worker.setMessenger(messenger);
+    worker.setTaskHandlerResolver((jt) -> (t) -> {
+      throw new IllegalArgumentException("bad input");
+    });
+    worker.handle(new MutableJobTask(Collections.EMPTY_MAP));
+  }
+  
+  
+}
