4339a8799c2519f90da43e2f9fea6a85e81b2446
creactiviti <arik.c.mail@gmail.com>
2017-04-03 22:35:35 -0700

    copyright banners.

--- src/main/java/com/creactiviti/piper/config/CoordinatorConfiguration.java
+++ src/main/java/com/creactiviti/piper/config/CoordinatorConfiguration.java
@@ -1,3 +1,10 @@
+/* 
+ * Copyright (C) Creactiviti LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Arik Cohen <arik@creactiviti.com>, Mar 2017
+ */
+
 package com.creactiviti.piper.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
