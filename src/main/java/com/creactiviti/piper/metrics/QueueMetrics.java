2767160a3e962a50eac5e9f2d4e3f0f7d45d46e9
creactiviti <arik.c.mail@gmail.com>
2017-04-09 21:13:27 -0700

    refactoring

---
+++ src/main/java/com/creactiviti/piper/metrics/QueueMetrics.java
@@ -0,0 +1,51 @@
+package com.creactiviti.piper.metrics;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.endpoint.PublicMetrics;
+import org.springframework.boot.actuate.info.InfoContributor;
+import org.springframework.boot.actuate.metrics.Metric;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Component;
+
+import com.creactiviti.piper.config.ConditionalOnCoordinator;
+import com.rabbitmq.http.client.Client;
+import com.rabbitmq.http.client.domain.QueueInfo;
+
+
+/**
+ * a {@link InfoContributor} implementation which exposes any declared AMQP
+ * queues.
+ * 
+ * @author Arik Cohen
+ * @since Apt 8, 2017
+ */
+@Component
+@ConditionalOnCoordinator
+@ConditionalOnProperty(name="piper.messenger.provider", havingValue="amqp")
+public class QueueMetrics implements PublicMetrics {
+
+  private Client client;
+  
+  @Autowired
+  public void setClient(Client aClient) {
+    client = aClient;
+  }
+
+  @Override
+  public Collection<Metric<?>> metrics() {
+    List<QueueInfo> queues = client.getQueues();
+    return queues.stream().map(q-> {
+      return Arrays.asList(
+          new Metric<>("queues."+q.getName()+".messages.total",q.getTotalMessages()),
+          new Metric<>("queues."+q.getName()+".messages.unacknowledged",q.getMessagesUnacknowledged()),
+          new Metric<>("queues."+q.getName()+".messages.ready",q.getMessagesReady())
+      );
+    }).flatMap(List::stream).collect(Collectors.toList());
+  }
+
+}
