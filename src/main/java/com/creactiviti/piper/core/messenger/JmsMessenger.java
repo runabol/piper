21bb921c5362cecb4d1e75a04898912aa7caef72
creactiviti <arik.c.mail@gmail.com>
2017-04-11 10:04:48 -0700

    implemented retry on the JmsMessenger.

--- src/main/java/com/creactiviti/piper/core/messenger/JmsMessenger.java
+++ src/main/java/com/creactiviti/piper/core/messenger/JmsMessenger.java
@@ -6,8 +6,12 @@
  */
 package com.creactiviti.piper.core.messenger;
 
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.util.Assert;
+
+import com.creactiviti.piper.error.Retryable;
 
 public class JmsMessenger implements Messenger {
 
@@ -16,7 +20,18 @@ public class JmsMessenger implements Messenger {
   @Override
   public void send (String aRoutingKey, Object aMessage) {
     Assert.notNull(aRoutingKey,"routing key can't be null");
+    if(aMessage instanceof Retryable) {
+      Retryable r = (Retryable) aMessage;
+      delay(r.getRetryDelay());
+    }
     jmsTemplate.convertAndSend(aRoutingKey, aMessage);
+  }
+  
+  private void delay (int aValue) {
+    try {
+      TimeUnit.MILLISECONDS.sleep(aValue);
+    } catch (InterruptedException e) {
+    }
   }
 
   public void setJmsTemplate(JmsTemplate aJmsTemplate) {
