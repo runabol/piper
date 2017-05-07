a79c155202ba01199c3296650e526826d92b8b56
Arik Cohen <arik.x.cohen.-nd@disney.com>
2017-05-07 09:18:03 -0700

    javadocs

--- src/main/java/com/creactiviti/piper/core/task/PipelineTask.java
+++ src/main/java/com/creactiviti/piper/core/task/PipelineTask.java
@@ -8,12 +8,43 @@ package com.creactiviti.piper.core.task;
 
 public interface PipelineTask extends Task {
 
+  /**
+   * Get the numeric order of the task
+   * in the pipeline. 
+   * 
+   * @return int
+   */
   int getTaskNumber ();
   
+  /**
+   * Get the identifier name of the task.
+   * Task names are used for assigning the 
+   * output of one task so it can be later
+   * used by subsequent tasks.
+   * 
+   * @return String
+   */
   String getName ();
   
+  /**
+   * Get the human-readable description 
+   * of the task.
+   * 
+   * @return String
+   */
   String getLabel ();
   
+  /**
+   * Defines the name of the type of 
+   * node that the task execution will 
+   * be routed to. So for instance if the 
+   * node value is: "encoder", then the task
+   * will be routed to the "encoder" queue
+   * which is presumably subscribed to 
+   * by worker nodes of "encoder" type.
+   * 
+   * @return String
+   */
   String getNode ();
   
 }
--- src/main/java/com/creactiviti/piper/core/task/Task.java
+++ src/main/java/com/creactiviti/piper/core/task/Task.java
@@ -10,6 +10,13 @@ import com.creactiviti.piper.core.Accessor;
 
 public interface Task extends Accessor {
   
+  /**
+   * Get the type of the task. Type strings
+   * are mapped to {@link TaskHandler} implementations
+   * designed to handler that type of task. 
+   * 
+   * @return String
+   */
   String getType ();
   
 }
