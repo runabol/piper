ae9c1996e2ec2af0989acd508fb7c18801b93a69
Arik Cohen <arik.x.cohen.-nd@disney.com>
2017-07-04 21:17:47 -0400

    removed copyright notice

--- piper-core/src/main/java/com/creactiviti/piper/core/pipeline/SimplePipeline.java
+++ piper-core/src/main/java/com/creactiviti/piper/core/pipeline/SimplePipeline.java
@@ -1,9 +1,4 @@
-/* 
- * Copyright (C) Creactiviti LLC - All Rights Reserved
- * Unauthorized copying of this file, via any medium is strictly prohibited
- * Proprietary and confidential
- * Written by Arik Cohen <arik@creactiviti.com>, Mar 2017
- */
+
 package com.creactiviti.piper.core.pipeline;
 
 import java.util.Collections;
