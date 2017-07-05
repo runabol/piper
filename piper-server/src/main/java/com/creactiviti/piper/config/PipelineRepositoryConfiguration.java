033f59ba7d31fed07831bd805f29088e7b82e42a
Arik Cohen <arik.x.cohen.-nd@disney.com>
2017-07-05 13:05:59 -0400

    added file system based repo.

--- piper-server/src/main/java/com/creactiviti/piper/config/PipelineRepositoryConfiguration.java
+++ piper-server/src/main/java/com/creactiviti/piper/config/PipelineRepositoryConfiguration.java
@@ -3,6 +3,7 @@ package com.creactiviti.piper.config;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -10,10 +11,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.core.annotation.Order;
 
-import com.creactiviti.piper.core.pipeline.ResourceBasedPipelineRepository;
 import com.creactiviti.piper.core.pipeline.GitPipelineRepository;
 import com.creactiviti.piper.core.pipeline.PipelineRepository;
 import com.creactiviti.piper.core.pipeline.PipelineRepositoryChain;
+import com.creactiviti.piper.core.pipeline.ResourceBasedPipelineRepository;
 
 @Configuration
 @EnableConfigurationProperties(PiperProperties.class)
@@ -24,23 +25,28 @@ public class PipelineRepositoryConfiguration {
   PipelineRepositoryChain pipelineRepository (List<PipelineRepository> aRepositories) {
     return new PipelineRepositoryChain(aRepositories);
   }
-  
-  @Bean
-  @Order(2)
-  @ConditionalOnProperty(name="piper.pipeline-repository.git.enabled",havingValue="true")
-  GitPipelineRepository gitPipelineRepository (PiperProperties piperProperties) {
-    GitPipelineRepository gitPipelineRepository = new GitPipelineRepository();
-    gitPipelineRepository.setUrl(piperProperties.getPipelineRepository().getGit().getUrl());
-    gitPipelineRepository.setSearchPaths(piperProperties.getPipelineRepository().getGit().getSearchPaths());
-    return gitPipelineRepository;
-  }
-  
+     
   @Bean
   @Order(1)
   ResourceBasedPipelineRepository resourceBasedPipelineRepository () {
     return new ResourceBasedPipelineRepository();
   }
   
+  @Bean
+  @Order(2)
+  @ConditionalOnProperty(name="piper.pipeline-repository.filesystem.enabled",havingValue="true")
+  ResourceBasedPipelineRepository fileSystemBasedPipelineRepository (@Value("piper.pipeline-repository.filesystem.base-path") String aBasePath) {
+    return new ResourceBasedPipelineRepository(String.format("file:%s", aBasePath));
+  }
   
+  @Bean
+  @Order(3)
+  @ConditionalOnProperty(name="piper.pipeline-repository.git.enabled",havingValue="true")
+  GitPipelineRepository gitPipelineRepository (PiperProperties piperProperties) {
+    GitPipelineRepository gitPipelineRepository = new GitPipelineRepository();
+    gitPipelineRepository.setUrl(piperProperties.getPipelineRepository().getGit().getUrl());
+    gitPipelineRepository.setSearchPaths(piperProperties.getPipelineRepository().getGit().getSearchPaths());
+    return gitPipelineRepository;
+  }  
   
 }
