27f2a14894e4e1a3c46af185e093cb66333514e6
Arik Cohen <arik.x.cohen.-nd@disney.com>
2017-07-05 12:56:18 -0400

    updated the scripts

--- scripts/rabbit.sh
+++ scripts/rabbit.sh
@@ -1,3 +1,7 @@
 #!/bin/bash 
 
-docker run -d --name=rabbit -p 5672:5672 -p 15672:15672 creactiviti/rabbitmq:3.6.9-management
+docker run -d \
+	   --name=rabbit \
+	   -p 5672:5672 \
+	   -p 15672:15672 \
+	   creactiviti/rabbitmq:3.6.9-management
--- scripts/start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-curl -s -X POST -H Content-Type:application/json -d '{"pipelineId":"'"$1"'"}' http://localhost:8080/jobs
