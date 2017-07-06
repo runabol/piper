4a3654981ed043586b9514bfe6a956b263bbaf36
Arik Cohen <arik.x.cohen.-nd@disney.com>
2017-07-06 10:49:31 -0400

    enabled git by default and added video/ to search path.

--- scripts/development.sh
+++ scripts/development.sh
@@ -6,9 +6,9 @@ java -jar \
      -Dpiper.coordinator.enabled=true \
      -Dpiper.worker.enabled=true \
      -Dpiper.worker.subscriptions.tasks=5 \
-     -Dpiper.pipeline-repository.git.enabled=false \
+     -Dpiper.pipeline-repository.git.enabled=true \
      -Dpiper.pipeline-repository.git.url=git@github.com:creactiviti/piper-pipelines.git \
-     -Dpiper.pipeline-repository.git.search-paths=demo/ \
+     -Dpiper.pipeline-repository.git.search-paths=demo/,video/ \
      -Dpiper.pipeline-repository.filesystem.enabled=true \
      -Dpiper.pipeline-repository.filesystem.base-path=$HOME/piper/**/*.yaml \
      piper-server/target/piper-server-0.0.1-SNAPSHOT.jar
