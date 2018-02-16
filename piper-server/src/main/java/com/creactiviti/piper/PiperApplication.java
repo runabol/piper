6c7e4ae8e3345bd891b302ea530ed80b33dfd4d2
Daisuke MURAOKA <daisuke.muraoka.jp@gmail.com>
2018-02-16 11:36:29 +0900

    write pid file

--- piper-server/src/main/java/com/creactiviti/piper/PiperApplication.java
+++ piper-server/src/main/java/com/creactiviti/piper/PiperApplication.java
@@ -3,11 +3,14 @@ package com.creactiviti.piper;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.system.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class PiperApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PiperApplication.class, args);
+		SpringApplication springApplication = new SpringApplication(PiperApplication.class);
+		springApplication.addListeners(new ApplicationPidFileWriter());
+		springApplication.run(args);
 	}
 }
