2f10bddfbdf13d0866e0a4fb902ae035926233bb
creactiviti <arik.c.mail@gmail.com>
2018-05-06 18:15:24 -0400

    bug fix

--- piper-core/src/main/java/com/creactiviti/piper/core/taskhandler/io/FilePath.java
+++ piper-core/src/main/java/com/creactiviti/piper/core/taskhandler/io/FilePath.java
@@ -8,10 +8,10 @@ import com.creactiviti.piper.core.task.Task;
 import com.creactiviti.piper.core.task.TaskHandler;
 
 /**
- * Gets the path from a full filename, which excludes the prefix, and also excluding the final directory separator. 
+ * Gets the full path from a full filename, which is the prefix + path, and also excluding the final directory separator. 
  * 
- * This method will handle a file in either Unix or Windows format. The method is entirely text based, and 
- * returns the text before the last forward or backslash. 
+ * This method will handle a file in either Unix or Windows format. The method is entirely text based, 
+ * and returns the text before the last forward or backslash. 
  * 
  * @author Arik Cohen
  * @since May 6, 2018
@@ -21,7 +21,7 @@ public class FilePath implements TaskHandler<Object> {
 
   @Override
   public Object handle (Task aTask) {
-    return FilenameUtils.getPathNoEndSeparator(aTask.getRequiredString("filename"));
+    return FilenameUtils.getFullPathNoEndSeparator(aTask.getRequiredString("filename"));
   }
 
 }
