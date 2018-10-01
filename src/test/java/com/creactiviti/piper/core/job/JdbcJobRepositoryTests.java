0b759fa9a59ad581a27339394ccd4359d56b9c7d
Chris Camel <camel.christophe@gmail.com>
2018-10-01 18:28:19 +0200

    add unit tests for count of completed jobs (today / yesterday)

--- src/test/java/com/creactiviti/piper/core/job/JdbcJobRepositoryTests.java
+++ src/test/java/com/creactiviti/piper/core/job/JdbcJobRepositoryTests.java
@@ -16,7 +16,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.sql.DataSource;
 import java.text.SimpleDateFormat;
+import java.time.Instant;
 import java.util.Date;
+
+import static java.time.temporal.ChronoUnit.DAYS;
 
 
 @RunWith(SpringRunner.class)
@@ -81,6 +84,96 @@ public class JdbcJobRepositoryTests {
     jobRepository.merge(mjob);
     one = jobRepository.findOne("1");
     Assert.assertEquals("FAILED",one.getStatus().toString());  
+  }
+
+  @Test
+  public void test3 () {
+    // arrange
+    JdbcTaskExecutionRepository taskRepository = new JdbcTaskExecutionRepository();
+    taskRepository.setJdbcOperations(new NamedParameterJdbcTemplate(dataSource));
+    taskRepository.setObjectMapper(createObjectMapper());
+
+    JdbcJobRepository jobRepository = new JdbcJobRepository();
+    jobRepository.setJdbcOperations(new NamedParameterJdbcTemplate(dataSource));
+    jobRepository.setJobTaskRepository(taskRepository);
+
+    SimpleJob completedJobYesterday = new SimpleJob();
+    completedJobYesterday.setId("1");
+    completedJobYesterday.setPipelineId("demo:1234");
+    completedJobYesterday.setCreateTime(Date.from(Instant.now().minus(2, DAYS)));
+    completedJobYesterday.setStatus(JobStatus.COMPLETED);
+    jobRepository.create(completedJobYesterday);
+    completedJobYesterday.setEndTime(Date.from(Instant.now().minus(1, DAYS)));
+    jobRepository.merge(completedJobYesterday);
+
+    for(int i = 0; i < 5; i++) {
+        SimpleJob completedJobToday = new SimpleJob();
+        completedJobToday.setId("2."+i);
+        completedJobToday.setPipelineId("demo:1234");
+        completedJobToday.setCreateTime(Date.from(Instant.now().minus(1, DAYS)));
+        completedJobToday.setStatus(JobStatus.COMPLETED);
+        jobRepository.create(completedJobToday);
+        completedJobToday.setEndTime(new Date());
+        jobRepository.merge(completedJobToday);
+    }
+
+    SimpleJob runningJobToday = new SimpleJob();
+    runningJobToday.setId("3");
+    runningJobToday.setPipelineId("demo:1234");
+    runningJobToday.setCreateTime(new Date());
+    runningJobToday.setStatus(JobStatus.STARTED);
+    jobRepository.create(runningJobToday);
+
+    // act
+    int todayJobs = jobRepository.countCompletedJobsToday();
+
+    // assert
+    Assert.assertEquals(5, todayJobs);
+  }
+
+  @Test
+  public void test4 () {
+    // arrange
+    JdbcTaskExecutionRepository taskRepository = new JdbcTaskExecutionRepository();
+    taskRepository.setJdbcOperations(new NamedParameterJdbcTemplate(dataSource));
+    taskRepository.setObjectMapper(createObjectMapper());
+
+    JdbcJobRepository jobRepository = new JdbcJobRepository();
+    jobRepository.setJdbcOperations(new NamedParameterJdbcTemplate(dataSource));
+    jobRepository.setJobTaskRepository(taskRepository);
+
+    for(int i = 0; i < 5; i++) {
+        SimpleJob completedJobYesterday = new SimpleJob();
+        completedJobYesterday.setId("1."+i);
+        completedJobYesterday.setPipelineId("demo:1234");
+        completedJobYesterday.setCreateTime(Date.from(Instant.now().minus(2, DAYS)));
+        completedJobYesterday.setStatus(JobStatus.COMPLETED);
+        jobRepository.create(completedJobYesterday);
+        completedJobYesterday.setEndTime(Date.from(Instant.now().minus(1, DAYS)));
+        jobRepository.merge(completedJobYesterday);
+    }
+
+    SimpleJob runningJobYesterday = new SimpleJob();
+    runningJobYesterday.setId("2");
+    runningJobYesterday.setPipelineId("demo:1234");
+    runningJobYesterday.setCreateTime(Date.from(Instant.now().minus(1, DAYS)));
+    runningJobYesterday.setStatus(JobStatus.STARTED);
+    jobRepository.create(runningJobYesterday);
+
+    SimpleJob completedJobToday = new SimpleJob();
+    completedJobToday.setId("3");
+    completedJobToday.setPipelineId("demo:1234");
+    completedJobToday.setCreateTime(Date.from(Instant.now().minus(1, DAYS)));
+    completedJobToday.setStatus(JobStatus.COMPLETED);
+    jobRepository.create(completedJobToday);
+    completedJobToday.setEndTime(new Date());
+    jobRepository.merge(completedJobToday);
+
+    // act
+    int yesterdayJobs = jobRepository.countCompletedJobsYesterday();
+
+    // assert
+    Assert.assertEquals(5, yesterdayJobs);
   }
 
   private ObjectMapper createObjectMapper() {
