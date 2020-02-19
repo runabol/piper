1ef2990d310e3d262ce3ccaa78362a42a2ab96c6
Arik Cohen <me@arikcohen.com>
2020-02-19 15:52:04 -0500

    don't sent ffmpeg's entire output back.

--- src/main/java/com/creactiviti/piper/taskhandler/media/Ffmpeg.java
+++ src/main/java/com/creactiviti/piper/taskhandler/media/Ffmpeg.java
@@ -15,18 +15,14 @@
  */
 package com.creactiviti.piper.taskhandler.media;
 
-import java.io.File;
-import java.io.PrintStream;
 import java.util.List;
 
 import org.apache.commons.exec.CommandLine;
 import org.apache.commons.exec.DefaultExecutor;
-import org.apache.commons.exec.ExecuteException;
-import org.apache.commons.exec.PumpStreamHandler;
-import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
 
 import com.creactiviti.piper.core.task.Task;
 import com.creactiviti.piper.core.task.TaskHandler;
@@ -44,24 +40,15 @@ class Ffmpeg implements TaskHandler<Object> {
   private Logger log = LoggerFactory.getLogger(getClass());
 
   @Override
-  public Object handle(Task aTask) throws Exception {
+  public Object handle (Task aTask) throws Exception {
     List<String> options = aTask.getList("options", String.class);
     CommandLine cmd = new CommandLine ("ffmpeg");
     options.forEach(o->cmd.addArgument(o));
     log.debug("{}",cmd);
     DefaultExecutor exec = new DefaultExecutor();
-    File tempFile = File.createTempFile("log", null);
-    try (PrintStream stream = new PrintStream(tempFile);) {
-      exec.setStreamHandler(new PumpStreamHandler(stream));
-      int exitValue = exec.execute(cmd);
-      return exitValue!=0?FileUtils.readFileToString(tempFile):cmd.toString();
-    }
-    catch (ExecuteException e) {
-      throw new ExecuteException(e.getMessage(),e.getExitValue(), new RuntimeException(FileUtils.readFileToString(tempFile)));
-    }
-    finally {
-      FileUtils.deleteQuietly(tempFile);
-    }
+    int exitValue = exec.execute(cmd);
+    Assert.isTrue(exitValue == 0, "exit value: " + exitValue);
+    return null;
   }
 
 }
