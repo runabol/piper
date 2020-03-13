2faa431a69d8d395a4243ba98d02158c4f12c817
Arik Cohen <me@arikcohen.com>
2020-03-13 09:04:25 -0400

    whitespace

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,58 +1,58 @@
 drop table if exists job cascade;
 
 create table job (
-  id varchar(256) not null primary key,
-  status varchar(256) not null,
-  current_task int not null,
-  pipeline_id varchar(256) not null,
-  label varchar(256),
-  create_time timestamp not null,
-  start_time timestamp,
-  end_time timestamp,
-  tags text not null,
-  priority int not null,
-  inputs text not null,
-  webhooks text not null,
-  outputs text not null,
+  id                       varchar(256) not null primary key,
+  status                   varchar(256) not null,
+  current_task             int          not null,
+  pipeline_id              varchar(256) not null,
+  label                    varchar(256)     null,
+  create_time              timestamp    not null,
+  start_time               timestamp        null,
+  end_time                 timestamp        null,
+  tags                     text         not null,
+  priority                 int          not null,
+  inputs                   text         not null,
+  webhooks                 text         not null,
+  outputs                  text         not null,
   parent_task_execution_id varchar(256)
 );
 
-CREATE INDEX ON job (create_time);
-CREATE INDEX ON job (status);
+create index on job (create_time);
+create index on job (status);
 
 drop table if exists task_execution cascade;
 
 create table task_execution (
-  id varchar(256) not null primary key,
-  parent_id varchar(256) null,
-  status varchar(256) not null,
-  progress int not null,
-  job_id varchar(256) not null,
-  create_time timestamp not null,
-  start_time timestamp,
-  end_time timestamp,
-  serialized_execution jsonb not null,
-  priority int not null,
-  task_number int not null
+  id                   varchar(256) not null primary key,
+  parent_id            varchar(256)     null,
+  status               varchar(256) not null,
+  progress             int not          null,
+  job_id               varchar(256) not null,
+  create_time          timestamp    not null,
+  start_time           timestamp        null,
+  end_time             timestamp        null,
+  serialized_execution jsonb        not null,
+  priority             int          not null,
+  task_number          int          not null
 );
 
-CREATE INDEX ON task_execution (job_id);
+create index on task_execution (job_id);
 
 drop table if exists context cascade;
 
 create table context (
-  id varchar(256) not null primary key,
-  stack_id varchar(256) not null,
-  create_time timestamp not null,
-  serialized_context jsonb not null
+  id                 varchar(256) not null primary key,
+  stack_id           varchar(256) not null,
+  create_time        timestamp    not null,
+  serialized_context jsonb        not null
 );
 
-CREATE INDEX ON context (stack_id);
+create index on context (stack_id);
 
 drop table if exists counter cascade;
 
 create table counter (
-  id varchar(256) not null primary key,
-  create_time timestamp not null,
-  value bigint not null
+  id          varchar(256) not null primary key,
+  create_time timestamp    not null,
+  value       bigint       not null
 );
